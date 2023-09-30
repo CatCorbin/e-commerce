d0ee1fe0259ff6dc2772c5cf08befacc2277c420
Cat Adams <catcorbin0919@gmail.com>
2023-09-29 23:10:30 -0600

    keys

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "categoryId",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "productId",
+  otherKey: "tagId",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: ProductTag,
+  foreignKey: "tagId",
+  otherKey: "productId",
+});
 
 module.exports = {
   Product,
